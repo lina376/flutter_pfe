5124f0594a9d77997755a5c3c11b5a8daaa633fb
Lina <rebahlina8@gmail.com>
2026-02-17 22:01:36 +0100

    interface

--- lib/screens/connecter.dart
+++ lib/screens/connecter.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class connecter extends StatefulWidget {
@@ -46,17 +47,21 @@ class _connecterState extends State<connecter> {
                   ),
                 ),
               ),
-              Positioned(
-                top: 300,
-                left: 10,
-                child: Text(
-                  "Email",
-                  style: TextStyle(color: Colors.white, fontSize: 14),
-                ),
-              ),
+
               Stack(
                 children: [
                   Positioned(
+                    top: 300,
+                    left: 12,
+                    child: Text(
+                      "Email",
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                  Positioned(
+                    top: 320,
+                    left: 10,
+                    right: 10,
                     child: TextField(
                       decoration: InputDecoration(
                         hintText: "email@gmail.com",
@@ -71,22 +76,64 @@ class _connecterState extends State<connecter> {
                     ),
                   ),
                   Positioned(
-                    top: 400,
-                    left: 10,
+                    top: 380,
+                    left: 12,
                     child: Text(
-                      "Email",
+                      "Mot de passe",
                       style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
                   ),
-                  TextField(
-                    decoration: InputDecoration(
-                      hintText: "********",
-                      filled: true,
-                      fillColor: Colors.white, //pour arriere blanc
-                      border: OutlineInputBorder(
-                        gapPadding: 3.0,
-                        borderRadius: BorderRadius.circular(8),
-                        borderSide: BorderSide(width: 0.5),
+                  Positioned(
+                    top: 400,
+                    left: 10,
+                    right: 10,
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: "********",
+                        filled: true,
+                        fillColor: Colors.white, //pour arriere blanc
+                        border: OutlineInputBorder(
+                          gapPadding: 3.0,
+                          borderRadius: BorderRadius.circular(8),
+                          borderSide: BorderSide(width: 0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                  //mrigl
+                  Positioned(
+                    top: 490,
+                    right: 135,
+                    left: 135,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "connecter",
+                        style: TextStyle(
+                          color: const Color.fromARGB(136, 10, 11, 22),
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        shape: StadiumBorder(),
+                        backgroundColor: const Color.fromARGB(
+                          172,
+                          153,
+                          129,
+                          180,
+                        ),
+                        elevation: 1,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 700,
+                    left: 135,
+
+                    child: Text(
+                      "Mot de passe oblier",
+                      style: TextStyle(
+                        color: Colors.black,
+                        decoration: TextDecoration.underline,
                       ),
                     ),
                   ),
