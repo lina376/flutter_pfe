551dd9c75900ffae4180b9bc0dc31e9676113bf6
Lina <rebahlina8@gmail.com>
2026-02-25 12:53:22 +0100

    interface

--- lib/screens/rencontre.dart
+++ lib/screens/rencontre.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ora/screens/connecter.dart';
 
 class rencontre extends StatefulWidget {
   const rencontre({super.key});
@@ -12,24 +13,6 @@ class _rencontreState extends State<rencontre> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        actions: [
-          IconButton(
-            style: const ButtonStyle(
-              backgroundColor: WidgetStatePropertyAll<Color>(
-                Color.fromARGB(194, 88, 70, 142),
-              ),
-            ),
-            icon: const Icon(Icons.home, color: Colors.white),
-            onPressed: () {},
-            tooltip: 'home',
-            iconSize: 40,
-            constraints: const BoxConstraints(minHeight: 50, minWidth: 50),
-          ),
-        ],
-      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
@@ -55,6 +38,168 @@ class _rencontreState extends State<rencontre> {
                   ),
                 ),
               ),
+              Positioned(
+                top: 20,
+                left: 58,
+                child: Text(
+                  "Rencontrez",
+                  style: TextStyle(
+                    fontSize: 55,
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 80,
+                left: 129,
+                child: Text(
+                  "ORA",
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.w800,
+                    color: Color(0xFF67A9FF),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 200,
+                left: 20,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 18,
+                    vertical: 12,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.12),
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(color: Colors.white.withOpacity(0.15)),
+                  ),
+                  child: const Text(
+                    "Avez-vous besoin\nd'aide?",
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 14,
+                      height: 1.2,
+                    ),
+                  ),
+                ),
+              ),
+              _buttomcommencer(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const connecter()),
+                  );
+                },
+              ),
+              Stack(
+                children: [
+                  Positioned(
+                    top: 265,
+                    left: 140,
+                    child: Container(
+                      width: 20,
+                      height: 20,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 279,
+                    left: 160,
+                    child: Container(
+                      width: 15,
+                      height: 15,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.15),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    top: 290,
+                    left: 180,
+                    child: Container(
+                      width: 10,
+                      height: 10,
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.3),
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white.withOpacity(0.15),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _buttomcommencer extends StatelessWidget {
+  final VoidCallback onTap;
+  const _buttomcommencer({required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 20,
+      right: 20,
+      left: 20,
+      child: GestureDetector(
+        onTap: () {},
+
+        child: Container(
+          height: 70,
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.10),
+            borderRadius: BorderRadius.circular(30),
+            border: Border.all(color: Colors.white.withOpacity(0.15)),
+          ),
+
+          child: Row(
+            children: [
+              const SizedBox(width: 10),
+
+              Container(
+                height: 44,
+                width: 44,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.18),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(Icons.arrow_forward, color: Colors.white),
+              ),
+              const SizedBox(width: 14),
+
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Commencer",
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.75),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(width: 44), //position de commencer
             ],
           ),
         ),
