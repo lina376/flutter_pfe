3cfbd533ed8815efb046735e7652ca25d1aa5bb8
Lina <rebahlina8@gmail.com>
2026-03-01 04:06:56 +0100

    interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-
-import 'package:google_fonts/google_fonts.dart';
 import 'package:ora/screens/chat.dart';
 import 'package:ora/screens/connecter.dart';
 import 'package:ora/screens/creecompte.dart';
