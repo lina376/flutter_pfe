ba6a9317859ca8485495b25a05a1d3ba568bc885
Lina <rebahlina8@gmail.com>
2026-03-31 17:20:36 +0100

    firebase favorise

--- lib/screens/favorise.dart
+++ lib/screens/favorise.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:ora/screens/principal.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ora/screens/notes2.dart';
